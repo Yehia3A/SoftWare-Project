f6cb73bdde4c79ebf233cb69ee0a19f0cd22c89b
dersleepy <mostafa.mosalam04@gmail.com>
2024-11-23 15:37:26 +0200

    changes in modules.module.ts to register the schema? idk

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ModuleSchema } from './modules.schema'; // Import the schema
 import { ModulesService } from './modules.service';
 import { ModulesController } from './modules.controller';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Module', schema: ModuleSchema }]), // Register the schema
+  ],
   providers: [ModulesService],
-  controllers: [ModulesController]
+  controllers: [ModulesController],
+  exports: [ModulesService], // Expose the service if needed by other modules
 })
 export class ModulesModule {}
