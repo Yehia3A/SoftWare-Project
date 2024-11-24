ab5223081f9e6df8743c1df3d057649abaee9eab
dersleepy <mostafa.mosalam04@gmail.com>
2024-11-24 11:10:52 +0200

    Updated ModulesController and ModulesService

--- src/modules/modules.controller.ts
+++ src/modules/modules.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { ModulesService } from './modules.service';
 
 @Controller('modules')
-export class ModulesController {}
+export class ModulesController {
+  constructor(private readonly modulesService: ModulesService) {}
+
+  @Get()
+  findAll() {
+    return this.modulesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.modulesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createModuleDto: any) {
+    return this.modulesService.create(createModuleDto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateModuleDto: any) {
+    return this.modulesService.update(id, updateModuleDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.modulesService.remove(id);
+  }
+}
--- src/modules/modules.service.ts
+++ src/modules/modules.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ModulesService {}
+export class ModulesService {
+  private modules = []; // This simulates a database or in-memory data storage
+
+  findAll() {
+    return this.modules; // Return all modules
+  }
+
+  findOne(id: string) {
+    return this.modules.find((module) => module.id === id); // Find a module by ID
+  }
+
+  create(createModuleDto: any) {
+    const newModule = { id: Date.now().toString(), ...createModuleDto }; // Assign a unique ID
+    this.modules.push(newModule);
+    return newModule; // Return the newly created module
+  }
+
+  update(id: string, updateModuleDto: any) {
+    const moduleIndex = this.modules.findIndex((module) => module.id === id);
+    if (moduleIndex === -1) {
+      return { message: 'Module not found' };
+    }
+    this.modules[moduleIndex] = { ...this.modules[moduleIndex], ...updateModuleDto };
+    return this.modules[moduleIndex]; // Return the updated module
+  }
+
+  remove(id: string) {
+    const moduleIndex = this.modules.findIndex((module) => module.id === id);
+    if (moduleIndex === -1) {
+      return { message: 'Module not found' };
+    }
+    const deletedModule = this.modules.splice(moduleIndex, 1);
+    return deletedModule[0]; // Return the deleted module
+  }
+}
