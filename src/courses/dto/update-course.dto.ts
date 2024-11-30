432abb90654d256ef29cca5b1d18c511b86a0e05
Mariam Fayed <mariamtfayed2134@gmail.com>
2024-11-30 19:01:44 +0200

    Changed updates
    added attribute that can be most likely changed when needing to update

--- src/courses/dto/update-course.dto.ts
+++ src/courses/dto/update-course.dto.ts
@@ -1,5 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types'; // allows one attribute to be changed if needed
 import { CreateCourseDto } from './create-course.dto';
+import { IsNotEmpty } from 'class-validator';
 
-export class UpdateCourseDto extends PartialType(CreateCourseDto) { } 
-//nothing added since no new field will be added
+export class UpdateCourseDto extends PartialType(CreateCourseDto) { 
+    @IsNotEmpty()
+    updatedAt : Date;
+} 
--- src/modules/dto/update-module.dto.ts
+++ src/modules/dto/update-module.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateModuleDto } from "../../modules/dto/create-module.dto";
+import { IsNotEmpty } from 'class-validator';
 
-export class UpdateCourseDto extends PartialType(CreateModuleDto){}
+export class UpdateCourseDto extends PartialType(CreateModuleDto){
+    @IsNotEmpty()
+    updatedAt : Date;
+}
